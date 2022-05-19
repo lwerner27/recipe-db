227cf363d197400411b5473671987c767a06106e
Lucas Werner <lwerner27@gmail.com>
2022-05-18 19:31:39 -0500

    Removed components that are not needed.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { Link, Routes, Route } from "react-router-dom";
 import AddRecipe from "./Pages/AddRecipe";
 import Login from "./Pages/Login";
-import Profile from "./Pages/Profile";
 
 export default function App() {
     return (
         <div className='=justify-center'>
             <nav className='py-5 text-center bg-slate-700 text-slate-300'>
                 <Link to='login'>LOGIN</Link> |{" "}
-                <Link to='profile'>PROFILE</Link> |{" "}
                 <Link to='add-recipe'>ADD RECIPE</Link>
             </nav>
 
             <Routes>
                 <Route path='login' element={<Login />} />
-                <Route path='profile' element={<Profile />} />
                 <Route path='add-recipe' element={<AddRecipe />} />
                 <Route path='default' element={<Login />} />
             </Routes>
--- src/Pages/Profile.js
+++
@@ -1,5 +0,0 @@
-const Profile = () => {
-    return <h1>This is the profile page.</h1>;
-};
-
-export default Profile;
